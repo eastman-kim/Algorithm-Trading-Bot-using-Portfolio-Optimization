18c83a0b73fe6931c4d79a18e5ac96f5c5e8a9f6
simjh96 <simjh96@yonsei.ac.kr>
2020-09-23 16:19:07 +0900

    waitting until keep buying == stop

--- MyKiwoom2.py
+++ MyKiwoom2.py
@@ -19,7 +19,7 @@ class Kiwoom(QAxWidget):
     def _set_signal_slots(self):
         self.OnEventConnect.connect(self._event_connect)
         self.OnReceiveTrData.connect(self._receive_tr_data)
-        # self.OnReceiveChejanData.connect(self._receive_chejan_data)
+        self.OnReceiveChejanData.connect(self._receive_chejan_data)
     
     #main에서 로그인 할때 쓰는 method
     def comm_connect(self):
@@ -57,16 +57,60 @@ class Kiwoom(QAxWidget):
     # 모의 투자는 주식 바로 입금 돼서 안쓰는 기능
 
     # fid = 체결잔고 아이템
-    # def get_chejan_data(self, fid):
-    #     ret = self.dynamicCall("GetChejanData(int)", fid)
+    def get_chejan_data(self, fid):
+        ret = self.dynamicCall("GetChejanData(int)", fid)
 
-    # def _receive_chejan_data(self, gubun, item_cnt, fid_list):
-    #     print(gubun)
+    def _receive_chejan_data(self, gubun, item_cnt, fid_list):
 
-    #     print(self.get_chejan_data(9203))
-    #     print(self.get_chejan_data(302))
-    #     print(self.get_chejan_data(900))
-    #     print(self.get_chejan_data(901))
+        self.chejan_gubun = int(gubun)
+        print("0:주문체결통보, 1:잔고통보, 3:특이신호 :",gubun)
+
+        print("주문번호",self.get_chejan_data(9203))
+        print("종목명",self.get_chejan_data(302))
+        print("주문수량",self.get_chejan_data(900))
+        print("주문가격",self.get_chejan_data(901))
+        
+        
+        if int(gubun) == 0: #구분도 맞고
+            for i in range(len(self.opw00018_output["multi"])):
+                print("--{}-{}--".format(self.opw00018_output["multi"][i][0],list(code_dict.keys())[list(code_dict.values()).index(code_list[i])]))
+                #종목명도 같을때
+                if self.opw00018_output["multi"][i][0] == list(code_dict.keys())[list(code_dict.values()).index(code_list[i])]:
+                    print("종목명 같음")
+                    self.chejan_loop.exit()
+                    print("chejan loop exit")
+                    time.sleep(1)
+                    print("예수금 조회 시작")
+
+                    try:
+                        self.keep_buying_loop.exit()
+                    except AttributeError:
+                        pass
+
+
+                    # self.set_input_value("계좌번호", account_number)
+                    # self.set_input_value("비밀번호","")
+                    # self.set_input_value("비밀번호입력매체구분","")
+                    # self.set_input_value("조회구분","2")
+
+                    # self.reset_opw00018_output()
+                    # self.comm_rq_data("opw00018_req", "opw00018", 0, "0101")
+
+                    # print("--{}-{}--".format(int(self.opw00018_output["multi"][i][1]),self.opt_q))
+                    # if int(self.opw00018_output["multi"][i][1]) != self.opt_q:
+                    #     self.keep_buying = "keep_buying"
+                    #     self.keep_buying_loop = QEventLoop()
+                    #     self.keep_buying_loop.exec_()
+                    # else:
+                    #     if self.keep_buying == "keep_buying":
+                    #         self.keep_buying = "enough"
+                    #         self.keep_buying_loop.exit()
+                        
+
+
+
+        elif int(gubun) == 1:
+            print("                ㄴ             잔고통보")
 
     def _receive_tr_data(self, screen_no, rqname, trcode, record_name, next, unused1, unused2, unused3, unused4):
         if next == "2":
@@ -228,6 +272,7 @@ if __name__ == "__main__":
     account_number = kiwoom.get_login_info("ACCNO")
     account_number = account_number.split(";")[0]
     kiwoom.set_input_value("계좌번호", account_number)
+    kiwoom.account_number = account_number
 
     # Test
     i = 1
@@ -239,21 +284,78 @@ if __name__ == "__main__":
     # 확인필요: 단순 조회에서도 next 값을 2로 넣어서 연속 조회를 나타내나? 1은 실시간 조회라는데
     kiwoom.comm_rq_data("opt10001_req", "opt10001", 0, "0101")
 
-    code_list = ["000020","000040","000050"]
+    code_dict = {"동화약품":"000020","KR모터스":"000040","경방":"000050", "메리츠화재":"000060", 
+                "삼양홀딩스":"000070", "하이트진로":"000080","유한양행":"000100","CJ대한통운":"000120","두산":"000150","삼성전자":"005930"}
+    code_list = ["000020","000040","000050","000060","000070","000080","000100","000120","000150","005930"]
 
-    for c in code_list:
-        i += 1
-        print("--{}번째 값".format(i))
-        time.sleep(0.5)
-        kiwoom.set_input_value("종목코드", c)
+    # for c in code_list:
+    #     i += 1
+    #     print("--{}번째 값".format(i))
+    #     time.sleep(0.5)
+    #     kiwoom.set_input_value("종목코드", c)
 
-        #next 값 2일때는 에러 나네...
-        kiwoom.comm_rq_data("opt10001_req", "opt10001", 0, "0101")
-        
+    #     #next 값 2일때는 에러 나네...
+    #     kiwoom.comm_rq_data("opt10001_req", "opt10001", 0, "0101")
+
+    # test2 최초로 한번 opt_output 에 자료 넣어줘야함
+    # set input value(여러번) 항상 확인해서 넣고 commreq해야함
+    kiwoom.set_input_value("계좌번호", account_number)
+    kiwoom.set_input_value("비밀번호","")
+    kiwoom.set_input_value("비밀번호입력매체구분","")
+    kiwoom.set_input_value("조회구분","2")
+
+    kiwoom.reset_opw00018_output()
+    kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "0101")
+    print("===================기존======================")
+    print(kiwoom.opw00018_output['single'])
+    print(kiwoom.opw00018_output['multi'])
+
+
+
     # test4 send order
     # (1:신규매수, 2:신규매도, 3:매수취소, 4:매도취소, 5:매수정정, 6:매도정정
-    kiwoom.send_order("order_req", "0101", account_number, "1", "005930", "10", 0, "03", "")
-    time.sleep(5)   #체결까지 시간 좀 걸림
+    debug_idx = 0
+    buy_q = 10
+    for i in range(len(code_list)):
+        print("===========================\n{}. ordering {}:{}\n===========================".format(
+            i,code_list[i],list(code_dict.keys())[list(code_dict.values()).index(code_list[i])]))
+
+        #optimal quantity 계산 하고 가
+        for j in range(len(kiwoom.opw00018_output["multi"])):
+            print("--{}-{}--".format(kiwoom.opw00018_output["multi"][j][0], list(code_dict.keys())[list(code_dict.values()).index(code_list[i])]))
+            if kiwoom.opw00018_output["multi"][j][0] == list(code_dict.keys())[list(code_dict.values()).index(code_list[i])]:  #종목명도 같을때
+                kiwoom.opt_q = int(kiwoom.opw00018_output["multi"][j][1]) + buy_q
+                print("optimal quantity = ", kiwoom.opt_q)
+
+        kiwoom.send_order("order_req", "0101", account_number, "1", code_list[i], buy_q, 0, "03", "")
+        # time.sleep(0.5)   #체결까지 시간 좀 걸림
+        
+        kiwoom.chejan_loop = QEventLoop()
+        kiwoom.chejan_loop.exec_()
+
+        kiwoom.set_input_value("계좌번호", account_number)
+        kiwoom.set_input_value("비밀번호","")
+        kiwoom.set_input_value("비밀번호입력매체구분","")
+        kiwoom.set_input_value("조회구분","2")
+
+        kiwoom.reset_opw00018_output()
+        kiwoom.comm_rq_data("opw00018_req", "opw00018", 0, "0101")
+
+        print("--{}-{}--".format(int(kiwoom.opw00018_output["multi"][i][1]),kiwoom.opt_q))
+        if int(kiwoom.opw00018_output["multi"][i][1]) != kiwoom.opt_q:
+            kiwoom.keep_buying = "keep_buying"
+            kiwoom.keep_buying_loop = QEventLoop()
+            kiwoom.keep_buying_loop.exec_()
+
+        
+
+
+
+        # if kiwoom.chejan_gubun != 0:
+        #     print("============================\n{} 번째".format(debug_idx))
+        #     print("error, 재주문\n===========================")
+        #     debug_idx += 1
+        #     i -= 1
 
     # test2
     # set input value(여러번) 항상 확인해서 넣고 commreq해야함
